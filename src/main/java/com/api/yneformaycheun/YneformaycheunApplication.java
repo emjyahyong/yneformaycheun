44f9f2812544aec031e97585d4123db3bf187126
Emjy <emjy.ahyong@gmail.com>
2026-07-13 14:20:53 +0400

    init

---
+++ src/main/java/com/api/yneformaycheun/YneformaycheunApplication.java
@@ -0,0 +1,13 @@
+package com.api.yneformaycheun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YneformaycheunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YneformaycheunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/yneformaycheun/YneformaycheunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.yneformaycheun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YneformaycheunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
